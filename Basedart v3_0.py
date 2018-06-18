5a3e4a9edc22d9ea4710ae80398082599bad61c2
Björn Veit <bveit@uni-bremen.de>
2018-06-18 14:02:51 +0200

    V 3.0 - Now with Bases

--- Basedart v3_0.py
+++ Basedart v3_0.py
@@ -1,5 +1,6 @@
 import random
 import math
+import time
 from tkinter import *
 
 def getNewHitter():
@@ -52,67 +53,209 @@ def UpdateTop():
     ChangeNumber('Out', Outs)
     ChangeNumber('Hitter', Hitter)
 
+def MoveOnBases(n):
+    #Move
+    global Bases
+    j = n
+    Bases = [True] + Bases
+    while( j != 1):
+        j -= 1
+        Bases = [False] + Bases
+
+    #Check if people got home
+    for i in range(3, n+3):
+        if(Bases[i]):
+            global Runs
+            Runs += 1
+            global Inning
+            RunsPerInning[Inning-1] += 1
+            ChangeNumber('Runs', Runs)
+            ChangeNumber('Inning%d' % Inning, RunsPerInning[Inning-1])
+
+    Bases = Bases[0:3]
+    updateBasePic()
+
+
+def updateBasePic():
+    #load new Picture
+    Name = "Bases/"
+    i = 0
+    for b in Bases:
+        i += 1
+        if b :
+            Name += "%d" %i
+
+    if(Name == "Bases/"):
+        Name += "Empty"
+    Name += ".gif"
+
+    global BasesPic    
+    BasesPic = PhotoImage(file=Name)
+    BasesLabel.config(image = BasesPic)
+    BasesFrame.update()
+    
+    
+def walk():
+    if(Bases[0]):
+        if(Bases[1]):
+            if(Bases[2]):
+                MoveOnBases(1)
+                return
+            Bases[2] = True
+        Bases[1] = True
+    Bases[0] = True
+    updateBasePic()
+
+    
 def strike():
+    StrikeBtn['state']  = 'disabled'
+    MissBtn['state']  = 'disabled'
     global Strikes    
     global Misses
     global PitchCount
-    
-    Strikes += 1    
+
+    HRProb = 0.01
+    DProb  = 0.02
+    SProb = 0.04
+    bHit = True
+    i = random.random()
+    print(i)
+    if(i <= HRProb):
+        #HR
+        PlayAnimation(SZAnimations['HR'])
+        MoveOnBases(4)
+    elif (i <= HRProb + DProb):
+        # double
+        PlayAnimation(SZAnimations['Double'])
+        MoveOnBases(2)
+    elif (i <= SProb + HRProb + DProb):
+        # single
+        PlayAnimation(SZAnimations['Single'])
+        MoveOnBases(1)
+    else:
+        # Strike
+        PlayAnimation(SZAnimations['Strike'])
+        Strikes += 1
+        bHit = False
+     
     PitchCount += 1
     
-    if(Strikes == 3):
+    if(bHit):
         Strikes = 0
         Misses = 0
         
         global Hitter
         Hitter = getNewHitter()
+    CheckAndHandleStrikeout()
+    UpdateTop()
+    if(GAMERUNNING):
+        StrikeBtn['state']  = 'normal'
+        MissBtn['state']  = 'normal'
+
+def miss():
+    
+    StrikeBtn['state']  = 'disabled'
+    MissBtn['state']  = 'disabled'
+    global Misses
+    global Strikes
+    global PitchCount
+
+    HRProb = 0.03
+    DProb  = 0.05
+    SProb = 0.10
+    StrikeProb = 0.07
+    
+    
+    bHit = True
+    i = random.random()
+    print(i)
+    if(i <= HRProb):
+        #HR
+        PlayAnimation(BAnimations['HR'])
+        MoveOnBases(4)
+    elif (i <= HRProb + DProb):
+        # double
+        PlayAnimation(BAnimations['Double'])
+        MoveOnBases(2)
+    elif (i <= SProb + HRProb + DProb):
+        # single
+        PlayAnimation(BAnimations['Single'])
+        MoveOnBases(1)        
+    elif (i <= SProb + HRProb + DProb + StrikeProb):
+        # Strike        
+        PlayAnimation(BAnimations['Strike'])
+        Strikes += 1
+        bHit = False
+    else:
+        # Ball
+        PlayAnimation(BAnimations['Ball'])
+        bHit = False       
+        Misses += 1
+    
+    PitchCount += 1
+    if(Misses == 4 or bHit):
+        if(Misses == 4):
+            walk()
+        Strikes = 0
+        Misses = 0
+        global Hitter
+        Hitter = getNewHitter()
+        
+    UpdateTop()
+    if(GAMERUNNING):
+        StrikeBtn['state']  = 'normal'
+        MissBtn['state']  = 'normal'
+
+def PlayAnimation(FileName) :
+  FrameNumber = 24
+  counter = 0
+  global AnimationPic
+
+  while counter < FrameNumber :
+    
+    AnimationPic = PhotoImage(file=FileName, format="gif -index " + str(counter))
+    AnimationLabel.config(image = AnimationPic)
+    AnimationFrame.update()
+    counter += 1
+    
+def CheckAndHandleStrikeout():
+    global Strikes
+    global Misses
+    if(Strikes == 3):
+        global Hitter
+        Strikes = 0
+        Misses = 0
         global Outs
         Outs += 1
         if(Outs == 3):
+            global Bases
+            Bases = [False, False, False]
+            updateBasePic()
             global Inning
             if(Inning == MaxInnings):
                 #Game End
+                global GAMERUNNING
+                GAMERUNNING = False
+                
                 StrikeBtn['state']  = 'disabled'
                 MissBtn['state']  = 'disabled'
 
-                #These will produce off
                 Hitter = OFF
                 Misses = OFF
                 Strikes = OFF
                 Outs = OFF
                 UpdateTop()
                 return
-                
+                            
             else:
                 Inning += 1
                 RunsPerInning.append(0)
                 ChangeNumber('Inning%d' % Inning, RunsPerInning[Inning-1])
-            
+                        
             
             Outs = 0
-
-    UpdateTop()  
-
-def miss():
-    global Misses
-    global Strikes
-    global PitchCount
-    
-    PitchCount += 1
-    Misses += 1
-    if(Misses == 4):
-        Strikes = 0
-        Misses = 0
-        global Runs
-        Runs += 1
-        global Inning
-        RunsPerInning[Inning-1] += 1
-        ChangeNumber('Runs', Runs)
-        ChangeNumber('Inning%d' % Inning, RunsPerInning[Inning-1])
-        global Hitter
+        
         Hitter = getNewHitter()
-    UpdateTop()  
-
     
 #main
 
@@ -131,23 +274,38 @@ Hitter = getNewHitter()
 PitchCount = 0
 RunsPerInning = [0]
 
+Bases = [False, False, False]
+
 #
 # Setup Window
 #
 window.title('Basedart')
-frame1 = Frame(window)
-frame1.grid(row=0,column=0, sticky=W)
-frame2 = Frame(window)
-frame2.grid(row=1,column=0, sticky=W)
-frame2_1 = Frame(frame2)
-frame2_1.grid(row=1,column=0, sticky=W)
-frame2_2 = Frame(frame2)
-frame2_2.grid(row=1,column=1, sticky=W)
-frame3 = Frame(window)
-frame3.grid(row=2,column=0, sticky=W)
+window.configure(background='white')
+Boardframe1 = Frame(window)
+Boardframe1.grid(row=0,column=0, sticky=W)
+Boardframe2 = Frame(window)
+Boardframe2.grid(row=1,column=0, sticky=W)
+Boardframe2_1 = Frame(Boardframe2)
+Boardframe2_1.grid(row=1,column=0, sticky=W)
+Boardframe2_2 = Frame(Boardframe2)
+Boardframe2_2.grid(row=1,column=1, sticky=W)
+Boardframe3 = Frame(window)
+Boardframe3.grid(row=2,column=0, sticky=W)
 
-Frames = [frame1, frame2_1, frame2_2, frame3]
-PicLabels = []
+Frames = [Boardframe1, Boardframe2_1, Boardframe2_2, Boardframe3]
+
+BottomFrame = Frame(window)
+BottomFrame.grid(row=3, column=0, sticky=W)
+BottomFrame.configure(background='white')
+
+AnimationFrame = Frame(BottomFrame)
+AnimationFrame.grid(row=0, column=0, sticky=W)
+
+BasesFrame = Frame(BottomFrame)
+BasesFrame.grid(row=0, column=1, sticky=W)
+BasesFrame.configure(background='white')
+
+
 #Backgorund
 BG00 = PhotoImage(file="Board/0_0.gif")
 BG01 = PhotoImage(file="Board/0_1.gif")
@@ -182,36 +340,62 @@ N8 = PhotoImage(file="Numbers/8.gif")
 N9 = PhotoImage(file="Numbers/9.gif")
 NumberImages = [N0, N1, N2, N3, N4, N5, N6, N7, N8, N9, Noff]
 
-Label(frame1, image=BG00, border=0).grid(row=0, column=0, sticky=S)
-Label(frame1, image=BG01, border=0).grid(row=0, column=1, sticky=W)
-Label(frame1, image=BG02, border=0).grid(row=0, column=2, sticky=W)
-Label(frame1, image=BG03, border=0).grid(row=0, column=3, sticky=W)
-Label(frame1, image=BG04, border=0).grid(row=0, column=4, sticky=W)
-Label(frame1, image=BG05, border=0).grid(row=0, column=5, sticky=W)
+#Animations
 
-Label(frame2_1, image=BG10, border=0).grid(row=0, column=0, columnspan=4, sticky=W)
+#
+AnimationPic = PhotoImage(file="Animation/SZStrike.gif")
+BasesPic = PhotoImage(file="Bases/Empty.gif")
 
-Label(frame2_2, image=BG11, border=0).grid(row=0, column=1, sticky=W)
-Label(frame2_2, image=BG12, border=0).grid(row=0, column=2, sticky=W)
-Label(frame2_2, image=BG13, border=0).grid(row=0, column=3, sticky=W)
-Label(frame2_2, image=BG14, border=0).grid(row=0, column=4, sticky=W)
-Label(frame2_2, image=BG15, border=0).grid(row=0, column=5, sticky=W)
-Label(frame2_2, image=BG16, border=0).grid(row=0, column=6, sticky=W)
-Label(frame2_2, image=BG17, border=0).grid(row=0, column=7, sticky=W)
-Label(frame2_2, image=BG18, border=0).grid(row=0, column=8, sticky=W)
-Label(frame2_2, image=BG19, border=0).grid(row=0, column=9, sticky=W)
-Label(frame2_2, image=BG110, border=0).grid(row=0, column=10, sticky=W)
-Label(frame3, image=BG20, border=0).grid(row=0, column=0, sticky=W)
+SZAnimations = {'Strike' : "Animation/SZStrike.gif",
+                'Single' : "Animation/SZSingle.gif",
+                'Double' : "Animation/SZDouble.gif",
+                'HR'     : "Animation/SZHomeRun.gif"}
+
+BAnimations = { 'Strike' : "Animation/BStrike.gif",
+                'Single' : "Animation/BSingle.gif",
+                'Double' : "Animation/BDouble.gif",
+                'HR'     : "Animation/BHomeRun.gif",
+                'Ball'   : "Animation/BBall.gif"}
+
+# Picture Labels
+Label(Boardframe1, image=BG00, border=0).grid(row=0, column=0, sticky=S)
+Label(Boardframe1, image=BG01, border=0).grid(row=0, column=1, sticky=W)
+Label(Boardframe1, image=BG02, border=0).grid(row=0, column=2, sticky=W)
+Label(Boardframe1, image=BG03, border=0).grid(row=0, column=3, sticky=W)
+Label(Boardframe1, image=BG04, border=0).grid(row=0, column=4, sticky=W)
+Label(Boardframe1, image=BG05, border=0).grid(row=0, column=5, sticky=W)
+
+Label(Boardframe2_1, image=BG10, border=0).grid(row=0, column=0, columnspan=4, sticky=W)
+
+Label(Boardframe2_2, image=BG11, border=0).grid(row=0, column=1, sticky=W)
+Label(Boardframe2_2, image=BG12, border=0).grid(row=0, column=2, sticky=W)
+Label(Boardframe2_2, image=BG13, border=0).grid(row=0, column=3, sticky=W)
+Label(Boardframe2_2, image=BG14, border=0).grid(row=0, column=4, sticky=W)
+Label(Boardframe2_2, image=BG15, border=0).grid(row=0, column=5, sticky=W)
+Label(Boardframe2_2, image=BG16, border=0).grid(row=0, column=6, sticky=W)
+Label(Boardframe2_2, image=BG17, border=0).grid(row=0, column=7, sticky=W)
+Label(Boardframe2_2, image=BG18, border=0).grid(row=0, column=8, sticky=W)
+Label(Boardframe2_2, image=BG19, border=0).grid(row=0, column=9, sticky=W)
+Label(Boardframe2_2, image=BG110, border=0).grid(row=0, column=10, sticky=W)
+Label(Boardframe3, image=BG20, border=0).grid(row=0, column=0, sticky=W)
+
+AnimationLabel = Label(AnimationFrame, image=AnimationPic, border=0)
+AnimationLabel.grid(row=0, column=0, sticky=W)
+
+BasesLabel = Label(BasesFrame, image=BasesPic, border=0)
+BasesLabel.configure(background='white')
+BasesLabel.grid(row=0, column=0, sticky=W)
 
 #Buttons
-StrikeBtn = Button(frame2_1, text="STRIKE", width=6, padx=20, pady=20, command=strike)
+StrikeBtn = Button(Boardframe2_1, text="STRIKE", width=6, padx=20, pady=20, command=strike)
 StrikeBtn.grid(row=0, column=1, sticky=W)
-MissBtn = Button(frame2_1, text="BALL", width=6, padx=20, pady=20, command=miss)
+MissBtn = Button(Boardframe2_1, text="BALL", width=6, padx=20, pady=20, command=miss)
 MissBtn.grid(row=0, column=2,   sticky=W)
 
 UpdateTop()
 ChangeNumber('Inning1', 0)
 ChangeNumber('Runs', 0)
+GAMERUNNING = True
 
 window.mainloop()
 
